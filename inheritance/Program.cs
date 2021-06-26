61edb42c12176f51d1a6c189fd1550a243ac14c5
aimanshariff31 <86050998+aimanshariff31@users.noreply.github.com>
2021-06-26 15:03:06 +0530

    Add files via upload

---
+++ inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students _student = new Students("MCA", "sumanth4206");
+        }
+    }
+}
