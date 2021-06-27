12f0bbc3e7485f3f62e8c835ad95bf9cbdf2baed
aimanshariff31 <86050998+aimanshariff31@users.noreply.github.com>
2021-06-27 20:25:31 +0530

    Add files via upload

---
+++ EngineApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EngineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiCorEngine _harrier = new DiCorEngine();
+            TataCarEngine Harrier = new TataCarEngine(_harrier);
+            Harrier.Drive();
+            Harrier.Halt();
+            VeriCorEngine _nexon = new VeriCorEngine();
+            TataCarEngine Nexon = new TataCarEngine(_nexon);
+            Nexon.Drive();
+            Nexon.Halt();
+            MultiJetEngine _hexa = new MultiJetEngine();
+            TataCarEngine Hexa = new TataCarEngine(_hexa);
+            Hexa.Drive();
+            Hexa.Halt();
+            DiCorEngine _safari = new DiCorEngine();
+            TataCarEngine Safari = new TataCarEngine(_safari);
+            Safari.Drive();
+            Safari.Halt();
+
+
+
+        }
+    }
+}
---
+++ EngineApp/TataCarEngine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineApp
+{
+    public class TataCarEngine
+    {
+        Iengine _engine;
+
+        public TataCarEngine(Iengine engine)
+        {
+
+            this._engine = engine;
+        }
+
+        public void Drive()
+        {
+
+            _engine.Start();
+        }
+        public void Halt()
+        {
+
+            _engine.Stop();
+        }
+
+
+    }
+    public interface Iengine
+    {
+        void Start();
+        void Stop();
+    }
+
+    public class DiCorEngine : Iengine
+    {
+
+        public void Start() { }
+        public void Stop() { }
+    }
+
+    public class VeriCorEngine : Iengine
+    {
+
+        public void Start() { }
+        public void Stop() { }
+    }
+
+    public class MultiJetEngine : Iengine
+    {
+
+        public void Start() { }
+        public void Stop() { }
+
+    }
+    public class RevorTron : Iengine
+    {
+
+        public void Start() { }
+        public void Stop() { }
+    }
+
+}
