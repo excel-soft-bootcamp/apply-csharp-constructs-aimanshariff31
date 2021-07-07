3b8d0ba7facc48953a36d96cc927e63646592184
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-07-07 08:34:10 +0530

    Add files via upload

---
+++ BMICalculatorProgram/CheckBmiLib/ConsoleInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckBmiLib
+{
+    
+        public class ConsoleInput
+        {
+            public static double weight;
+            public static double height, bmi;
+            
+
+            public void displayinput()
+            {
+                Console.WriteLine("BMI Calculator");
+                Console.WriteLine("------------------");
+                Console.WriteLine("Weight in kg: ");
+                weight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Height in cm: ");
+                height = Convert.ToDouble(Console.ReadLine());
+
+            }
+
+
+           
+           
+        }
+    
+}
