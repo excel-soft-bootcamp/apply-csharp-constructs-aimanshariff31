17b4ee5eaba3ff7326f9eb55c4513b085ea92651
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-07-07 08:36:01 +0530

    Add files via upload

---
+++ BMICalculatorProgram/ValidateBmiLib/BmiValidation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidateBmiLib
+{
+    public class BmiValidation
+    {
+        public void ValidateBmiValue()
+        {
+            
+            double value = CalculateBmiLib.CalculateBmi.bmi;
+            
+            Display obj = new Display();
+            if (value < 18.5)
+            { obj.Underweight(); }
+            if (value >= 19 & value <= 24)
+            { obj.Healthy(); }
+            if (value >= 25)
+            { obj.Overweight(); }
+
+
+        }
+       
+    }
+    public class Display
+    {
+        public void Underweight()
+        {
+            Console.WriteLine("Underweight");
+        }
+        public void Healthy()
+        {
+            Console.WriteLine("Healthy");
+        }
+        public void Overweight()
+        {
+            Console.WriteLine("Overweight");
+        }
+    }
+}
